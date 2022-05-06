781c6deddeb5c5c18a1a3f30fde898ce737051e4
Omar Hesham <oommaarr.220022@gmail.com>
2022-05-06 07:35:56 +0200

    Added different options for debugging using POST or GET requests

--- debugger.py
+++ debugger.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 from tabulate import tabulate
 from Models.user import User
 from Models.log import Log 
@@ -15,12 +15,21 @@ def getLogs():
     """gets all logs in the system for debugging purpose"""
 
     logs = Log.query.all()
-    logs_table = []
 
-    for log in logs:
-        logs_table.append([log.id, log.username, log.unlocking_method, log.date_time])
+    if request.method == "GET":
+        logs_table = []
+        for log in logs: logs_table.append([log.id, log.username, log.unlocking_method, log.date_time])
+        return "<pre>" + tabulate(logs_table, headers=["ID","Username", "Unlocking Method", "Unlocking Date/Time"])  + "</pre>"
 
-    return "<pre>" + tabulate(logs_table, headers=["ID","Username", "Unlocking Method", "Unlocking Date/Time"])  + "</pre>"
+    else:
+        logs_dict = {}
+        for log in logs: logs_dict[log.id] = {
+            "id" : log.id, 
+            "username": log.username, 
+            "unlocking_method": log.unlocking_method, 
+            "date_time": log.date_time
+        }
+        return jsonify(logs_dict)
 
 
 @debugger.route("/getUsers", methods=["GET", "POST"])
@@ -28,9 +37,21 @@ def getUsers():
     """gets all users in the system for debugging purpose"""
 
     users = User.query.all()
-    users_table = []
 
-    for user in users:
-        users_table.append([user.id, user.public_id, user.username, user.password, user.phone, user.address])
+    if request.method == "GET":
+        users_table = []
+        for user in users: users_table.append([user.id, user.public_id, user.username, user.password, user.phone, user.address])
+        return "<pre>" + tabulate(users_table, headers=["ID", "Public ID", "Username", "Password", "Phone", "Address"]) + "</pre>"
 
-    return "<pre>" + tabulate(users_table, headers=["ID", "Public ID", "Username", "Password", "Phone", "Address"]) + "</pre>"
+    else:
+        users_dict = {}
+        for user in users: users_dict[user.id] = {
+            "id" : user.id,
+            "public_id" : user.public_id,
+            "username" : user.username,
+            "password" : user.password,
+            "phone" : user.phone,
+            "address" : user.address
+        }
+        return jsonify(users_dict)
+        
